7532b83ca056f75e05ae70538956c25b3887c26a
EgorDubovik <posik.web.m@gmail.com>
2022-12-09 21:17:50 -0600

    create accesses

---
+++ app/Models/Access.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Access extends Model
+{
+    use HasFactory;
+
+    protected $table = 'accesses';
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'creator_id',
+    ];
+}
